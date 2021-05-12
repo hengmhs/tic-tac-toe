711d507594d70c99ab8709cdb64272c7f35a7d43
zhaboots <zhaboots@users.noreply.github.com>
2021-05-13 01:46:20 +0800

    Add styles.css and table

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Gameboard Module
 
-let board = [['','',''],
-			 ['','',''],
-			 ['','','']]
+const gameBoard = (() => {
+	let board = [['X','O','X'],
+			 	 ['O','X','O'],
+			 	 ['','O','X']]
+	return { board };
+})();
+
+// displayController Module
+
+
+// Player factories
