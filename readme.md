ec5bb092f7a4148c0a9bd8e7a5a85a87d5aad50f
zhaboots <zhaboots@users.noreply.github.com>
2021-05-14 21:16:02 +0800

    Add Player factory, gameController, gameBoard, displayController modules

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ The Odin Project assignment to create a Tic-Tac-Toe game.
 ## Learning Points:
 
 1. Modules
+2. for ... of loops
+3. IIFE: Immediately Invoked Function Expressions
--- script.js
+++ script.js
@@ -1,13 +1,71 @@
+// Player factories
+
+const Player = (name, symbol) => {
+	return { name, symbol}
+}
+
+let p1 = Player('John', 'X');
+let p2 = Player('Jane', 'O');
+
+// gameController Module with IIFE -> Immediately Invoked Function Expression
+const gameController = (() => {
+	let currentPlayer = p1;
+	const nextTurn = () => {
+		if(currentPlayer === p1){
+			currentPlayer = p2;
+		} else {
+			currentPlayer = p1;
+		}
+	}
+	// IIFE variables have to be accessed via methods to see changes
+	// After nextTurn() is called, cannot directly access gameController.currentPlayer
+	// use gameController.getCurrent()
+	const getCurrent = () => {
+		return currentPlayer;
+	}
+	return { currentPlayer , nextTurn, getCurrent};
+})();
+
 // Gameboard Module
 
 const gameBoard = (() => {
-	let board = [['X','O','X'],
-			 	 ['O','X','O'],
-			 	 ['','O','X']]
-	return { board };
+	let board = [['1','2','3'],
+			 	 ['4','5','6'],
+			 	 ['7','8','9']]
+	const changeValue = (val, symbol) => {
+		board[val[0]][val[1]] = symbol;
+	}
+	const checkGameOver = () => {
+		
+	}
+	return { board, changeValue };
 })();
 
 // displayController Module
 
+const displayController = (() => {
+	// gets each td in displayBoard
+	let displayBoard = document.getElementById('displayBoard')
+	let tdList = displayBoard.querySelectorAll('td');
+	const clickSquare = (event) => {
+		// id of td element
+		const position = event.target.id;
+		gameBoard.changeValue(position, gameController.getCurrent().symbol);
+		gameController.nextTurn();
+		renderBoard();
+	}
+	const renderBoard = () => {
+		let counter = 0;
+		for(let row of gameBoard.board) {
+			for(let td of row){
+				tdList[counter].innerHTML = td;
+				tdList[counter].addEventListener('click', clickSquare);
+				counter++;
+			}
+		} 
+	}
+	return { displayBoard, renderBoard};
+})();
 
-// Player factories
+displayController.renderBoard();
+
